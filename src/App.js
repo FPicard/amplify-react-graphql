e652c49a530094b64ff8c54b733f416ac2924baa
François Picard <fpicard01@gmail.com>
2022-11-08 23:04:56 -0500

    Test FP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react-v1';
+import Amplify from 'aws-amplify';
+import logo from './logo.svg';
+import { AmplifyAuthenticator, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react-v1';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from './aws-exports';
 
+Amplify.configure(awsconfig);
 
-function App() {
-  return (
+ 
+const AuthStateApp = () => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
     <div className="App">
-      <header>
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello Marie Thérèse</h1>
-      </header>
+      
+	<div>Utilisateur logguer {user.username}</div>
       <AmplifySignOut />
+	  </header>
     </div>
-  );
-}
+  ) : 
+  
+  (
+        <header className="App-header">
 
-export default withAuthenticator(App);
+    <AmplifyAuthenticator>
+  <AmplifySignIn
+    headerText="Essai Connection Francois v1_17"
+    slot="sign-in"
+	formFields={[
+		  { type: 'username' },
+          {
+            type: 'password',
+            label: 'Test Encyptage Password',
+            placeholder: 'password',
+            inputProps: { required: true, autocomplete: 'password' },
+          },
+        ]}
+  ></AmplifySignIn>
+</AmplifyAuthenticator>
+	  </header>
+
+  );
+};
+
+
+
+
+export default AuthStateApp;
