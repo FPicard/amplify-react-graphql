982e86f080d9516f963e470f32e499134f579a69
François Picard <fpicard01@gmail.com>
2022-11-13 00:18:56 -0500

    Database

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,11 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://xpak4r4cffbo7jkfe2hhorysny.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-5mqahw2mtfertd6nr2elcgv7i4"
 };
 
 
