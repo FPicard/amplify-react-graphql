26d55f882da99ca028113d5768223cb6896fd9e2
François Picard <fpicard01@gmail.com>
2022-11-14 23:10:38 -0500

    CHange to adding Design UI5

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -110,7 +110,7 @@ async function createNote() {
 	  type="file"
 	  onChange={onChange}
 		/>
-      <Button variant="contained" color="primary" onClick={createNote}>Create Note</Button>
+      <Button variant="contained" color="primary" onClick={createNote}>Create Notes</Button>
       <div style={{marginBottom: 30}}>
         {
            notes.map(note => (
